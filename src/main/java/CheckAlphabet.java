fc426e2a41ae5f587cc805cf21da93975930f931
ariari01 <juh.shin@gmail.com>
2025-06-04 16:54:27 +0900

    feature 3

--- src/main/java/CheckAlphabet.java
+++ src/main/java/CheckAlphabet.java
@@ -1,4 +1,8 @@
 public class CheckAlphabet {
+
+    public static final int LENGTH_MATCH_MAX_POINT = 60;
+    public static final int DOUBLE_SIZE_ZERO_POINT = 0;
+
     public void validCheck(String word1, String word2) {
         if (word1 == null || word2 == null) {
             throw new  IllegalArgumentException();
@@ -6,9 +10,31 @@ public class CheckAlphabet {
     }
 
     public int lengthCheck(String word1, String word2) {
-        if (word1.length() == word2.length()) {
-            return 60;
+        if (isMatched(word1, word2)) {
+            return LENGTH_MATCH_MAX_POINT;
+        } else if (isDoubleSize(word1, word2)) {
+            return DOUBLE_SIZE_ZERO_POINT;
+        } else {
+            int gab;
+            int shortLength;
+            if (word1.length() > word2.length()) {
+                gab = word1.length() - word2.length();
+                shortLength = word2.length();
+            } else {
+                gab = word2.length() - word1.length();
+                shortLength = word1.length();
+            }
+            double ratio = (double) gab / shortLength;
+            double temp = (1 - ratio) * 60;
+            return (int)temp;
         }
-        return 0;
+    }
+
+    private boolean isDoubleSize(String word1, String word2) {
+        return (word1.length() >= (word2.length() * 2)) || (word2.length() >= (word1.length() * 2));
+    }
+
+    private boolean isMatched(String word1, String word2) {
+        return word1.length() == word2.length();
     }
 }
--- src/test/java/CheckAlphabetTest.java
+++ src/test/java/CheckAlphabetTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,11 +27,48 @@ public class CheckAlphabetTest {
     }
 
     @Test
+    @DisplayName("같은 사이즈")
     void sameLength() {
         String word1 = "ASD";
         String word2 = "DSA";
 
         int expected = 60;
+        int result = checkAlphabet.lengthCheck(word1, word2);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("두 배 이상 사이즈")
+    void doubleLegnth() {
+        String word1 = "BBB";
+        String word2 = "A";
+
+        int expected = 0;
+        int result = checkAlphabet.lengthCheck(word1, word2);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("부분점수")
+    void partialScore() {
+        String word1 = "AAABB";
+        String word2 = "BAA";
+
+        int expected = 20;
+        int result = checkAlphabet.lengthCheck(word1, word2);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("부분점수2")
+    void partialScore2() {
+        String word1 = "AA";
+        String word2 = "AAE";
+
+        int expected = 30;
         int result = checkAlphabet.lengthCheck(word1, word2);
 
         assertEquals(expected, result);
